bad32eeb513e39ef7acb6b5ed6b37b82b0552ef3
Zuhair Naqi <zuhair.naqi@iomechs.com>
2021-12-21 00:37:00 +0500

    feat: add created_at field when creating item and show timer show item list

--- index.js
+++ index.js
@@ -1,67 +1,75 @@
 function update() {
-    if (localStorage.getItem("itemsJson") == null) {
-      //   console.log("hy");
-      itemJsonArray = [];
-      //   itemJsonArray.push([tit, desc]);
-      localStorage.setItem("itemsJson", JSON.stringify(itemJsonArray));
-    } else {
-      //   console.log("bye");
-      itemJsonArrayStr = localStorage.getItem("itemsJson");
-      itemJsonArray = JSON.parse(itemJsonArrayStr);
-      //   itemJsonArray.push([tit, desc]);
-      //   localStorage.setItem("itemsJson", JSON.stringify(itemJsonArray));
-    }
-   
-    //populating the table
-    tableBody = document.getElementById("tableBody");
-    let str = "";
-    itemJsonArray.forEach((element, index) => {
-      str += `
-        <tr>
-          <th scope="row">${index + 1}</th>
-          <td>${element[0]}</td>
-          <td>${element[1]}</td>
-          <td><button class="btn btn-sm btn-primary" onclick="deleted(${index})">Delete</button></td>
-          <td id="countdown"></td>
-          </tr>
-        `;
-    });
-    tableBody.innerHTML = str;
-  }
-  add = document.getElementById("add");
-  add.addEventListener("click", getAndUpdate);
-  update();
-
-  function getAndUpdate() {
-    tit = document.getElementById("title").value;
-    desc = document.getElementById("description").value;
-    if (localStorage.getItem("itemsJson") == null) {
-      //   console.log("hy");
-      itemJsonArray = [];
-      itemJsonArray.push([tit, desc]);
-      localStorage.setItem("itemsJson", JSON.stringify(itemJsonArray));
-    } else {
-      //   console.log("bye");
-      itemJsonArrayStr = localStorage.getItem("itemsJson");
-      itemJsonArray = JSON.parse(itemJsonArrayStr);
-      itemJsonArray.push([tit, desc]);
-      localStorage.setItem("itemsJson", JSON.stringify(itemJsonArray));
-    }
-    update();
-  }
-
-  function deleted(itemIndex) {
-    console.log("Deleted", itemIndex);
+  if (localStorage.getItem("itemsJson") == null) {
+    //   console.log("hy");
+    itemJsonArray = [];
+    //   itemJsonArray.push([tit, desc]);
+    localStorage.setItem("itemsJson", JSON.stringify(itemJsonArray));
+  } else {
+    //   console.log("bye");
     itemJsonArrayStr = localStorage.getItem("itemsJson");
     itemJsonArray = JSON.parse(itemJsonArrayStr);
-    itemJsonArray.splice(itemIndex, 1);
-    localStorage.setItem("itemsJson", JSON.stringify(itemJsonArray));
-    update();
+    //   itemJsonArray.push([tit, desc]);
+    //   localStorage.setItem("itemsJson", JSON.stringify(itemJsonArray));
   }
 
-  function clearList() {
-    if (confirm("Do you really wanna clear whole list?"))
-      localStorage.clear();
-    update();
+  //populating the table
+  tableBody = document.getElementById("tableBody");
+  let str = "";
+  itemJsonArray.forEach((element, index) => {
+    str += `
+        <tr>
+          <th scope="row">${index + 1}</th>
+          <td>${element.title}</td>
+          <td>${element.description}</td>
+          <td><button class="btn btn-sm btn-primary" onclick="deleted(${index})">Delete</button></td>
+          <td class="created-at" id="${element.created_at}"></td>
+          </tr>
+        `;
+  });
+  tableBody.innerHTML = str;
+}
+
+add = document.getElementById("add");
+add.addEventListener("click", getAndUpdate);
+update();
+
+function getAndUpdate() {
+  const title = document.getElementById("title").value;
+  const description = document.getElementById("description").value;
+
+  itemJsonArray = JSON.parse(localStorage.getItem("itemsJson"));
+  const data = { title, description, created_at: Date.now() };
+  itemJsonArray.push(data);
+  localStorage.setItem("itemsJson", JSON.stringify(itemJsonArray));
+
+  update();
+}
+
+function deleted(itemIndex) {
+  console.log("Deleted", itemIndex);
+  itemJsonArrayStr = localStorage.getItem("itemsJson");
+  itemJsonArray = JSON.parse(itemJsonArrayStr);
+  itemJsonArray.splice(itemIndex, 1);
+  localStorage.setItem("itemsJson", JSON.stringify(itemJsonArray));
+  update();
+}
+
+function clearList() {
+  if (confirm("Do you really wanna clear whole list?")) localStorage.clear();
+  update();
+}
+
+setInterval(() => {
+  let createdAtItems = document.getElementsByClassName("created-at");
+  if (createdAtItems) {
+    for (let i = 0; i < createdAtItems.length; i++) {
+      const createdAtTime = Number(createdAtItems[i].id);
+      const timeLimit = createdAtTime + 24 * 60 * 60 * 1000;
+      const timeDifference = timeLimit - Date.now();
+      const hour = Math.floor(timeDifference / (60 * 60 * 1000));
+      const min = Math.floor((timeDifference / (60 * 1000)) % 60);
+      const sec = Math.floor((timeDifference / 1000) % 60);
+      createdAtItems[i].innerText = `${hour} : ${min} : ${sec}`;
+    }
   }
-  
+}, 1000);
